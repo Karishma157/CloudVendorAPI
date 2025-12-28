e02594ad87addf05fa5491af4b309090be98baad
Karishma157 <karishmaagrahari19@GMAIL.COM>
2025-12-28 19:06:38 +0530

    Add files via upload

---
+++ cloudVendorServiceImpl.java
@@ -0,0 +1,50 @@
+package com.thinkconstructive.rest_demo.service.impl;
+
+import com.thinkconstructive.rest_demo.model.CloudVendor;
+import com.thinkconstructive.rest_demo.repository.CloudVendorRepository;
+import com.thinkconstructive.rest_demo.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class cloudVendorServiceImpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+
+    public cloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudvendor) {
+        // More business logic
+        cloudVendorRepository.save(cloudvendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        // More business logic
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        // More business logic
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        // More business logic
+        return cloudVendorRepository.findAll();
+    }
+}
